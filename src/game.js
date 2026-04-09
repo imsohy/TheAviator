a8144ab51a98ec1b92c64ce6abf3726f9992c2c8
legacytachyon <legacytachyon@gmail.com>
2026-04-09 13:35:34 +0900

    Remove fog effect from scene in game, part1, and part2 files

--- src/game.js
+++ src/game.js
@@ -123,7 +123,7 @@ function createScene() {
   nearPlane = 0.1;
   farPlane = 10000;
   camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
-  scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
+  scene.fog = null;
   camera.position.x = 0;
   camera.position.z = 200;
   camera.position.y = game.planeDefaultHeight;
--- src/part1.js
+++ src/part1.js
@@ -32,7 +32,7 @@ function createScene() {
   nearPlane = 1;
   farPlane = 10000;
   camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
-  scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
+  scene.fog = null;
   camera.position.x = 0;
   camera.position.z = 200;
   camera.position.y = 100;
